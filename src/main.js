4399c246fde4bfec79ec9ab05439351feaa06f23
Annika Bürsner <annika.buersner@hm.edu>
2026-06-12 18:15:52 +0200

    Initial Camera set up

--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
 import './style.css';
 
-const app = document.getElementById('app');
+document.querySelector('#app').innerHTML = `
+  <section class="screen">
+    <video id="webcam" autoplay playsinline muted></video>
 
-app.innerHTML = `
-  <h1>HOW PERFECT ARE YOU?</h1>
+    <div class="dark-overlay"></div>
+
+    <header class="headline">
+      <h1>HOW PERFECT ARE YOU?</h1>
+    </header>
+
+    <div class="face-window"></div>
+  </section>
 `;
+
+const video = document.querySelector('#webcam');
+
+async function startCamera() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+
+    video.srcObject = stream;
+  } catch (error) {
+    console.error('Camera error:', error);
+  }
+}
+
+startCamera();
